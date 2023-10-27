cb2cbd2064e55c3ec4b11d7fc23b76407995cb58
Kirill Surin <k.5urin@yandex.ru>
2023-10-27 13:50:50 +0300

    Сделал работу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
+const templateCard = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
 
+function createCard(item, removeCard) {
+    const placesItem = templateCard.querySelector('.places__item').cloneNode(true);
+        placesItem.querySelector('.card__image').src = item.link;
+        placesItem.querySelector('.card__image').alt = item.name;
+        placesItem.querySelector('.card__title').textContent = item.name;
+
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', removeCard);
+    
+    return placesItem;
+};
+
+function addCard(item) {
+    cardList.append(item); 
+};
+
+// @todo: Функция удаления карточки
+function removeCard(evt) {
+    const deleteCard = evt.target.closest('.places__item');
+    deleteCard.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) { 
+    const createdCard = createCard(item, removeCard); 
+    addCard(createdCard);
+ });
+ 
